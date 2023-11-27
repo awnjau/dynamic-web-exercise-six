d0a084041f21970aaf88a53a88df03c048734c02
awnjau <98540454+awnjau@users.noreply.github.com>
2023-11-26 19:42:25 -0500

    updated

--- src/app/components/CreateUserForm.js
+++ src/app/components/CreateUserForm.js
@@ -1,7 +1,21 @@
+import styles from './components.module.css';
+
 const createUserForm = () =>{
     return(
         <div>
             <h2>Create User</h2>
+            <form className={styles.Form}>
+                <label htmlForm="name">Name</label>
+                <input type="text" name="name" />
+
+                <label htmlForm="email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlForm="pass">Password</label>
+                <input type="password" name="pass" />
+                
+                <button type = "submit">Create User</button>
+            </form>
         </div>
     );
 };
--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -1,7 +1,19 @@
+import styles from './components.module.css';
+
 const LoginForm = () =>{
     return(
         <div>
             <h2>Login Form</h2>
+            <form className={styles.Form}>
+
+                <label htmlForm="email">Email</label>
+                <input type="email" name="email" />
+
+                <label htmlForm="pass">Password</label>
+                <input type="password" name="pass" />
+                
+                <button type = "submit">Create User</button>
+            </form>
         </div>
     );
 };
